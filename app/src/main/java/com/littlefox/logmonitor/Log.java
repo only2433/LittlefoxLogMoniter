75d430e28f8de249f94eba0932d0391cb08195df
only2433 <only2433@naver.com>
2017-10-31 14:29:04 +0900

    save 3

--- app/src/main/java/com/littlefox/logmonitor/Log.java
+++ app/src/main/java/com/littlefox/logmonitor/Log.java
@@ -176,6 +176,7 @@ public final class Log {
     
 
     public static int f(String msg) {
+
 		String log = convertErrMsg("<I>" + msg);
 		setFileLog(log);
 		return android.util.Log.i(none_tag, log);
