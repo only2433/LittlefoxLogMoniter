f151ceaaf206d23bee36a976bb5b8b913e2459e9
only2433 <only2433@naver.com>
2018-04-10 15:01:41 +0900

    - 메모리 관리 관련 메세지 추가

--- app/src/main/java/com/littlefox/logmonitor/ExceptionCheckHandler.java
+++ app/src/main/java/com/littlefox/logmonitor/ExceptionCheckHandler.java
@@ -1,9 +1,7 @@
 package com.littlefox.logmonitor;
 
 import android.app.Activity;
-import android.app.ActivityManager;
 import android.content.Context;
-import android.os.Build;
 import android.os.Process;
 
 import com.littlefox.logmonitor.Log.LOG_TYPE;
@@ -25,21 +23,6 @@ public class ExceptionCheckHandler implements UncaughtExceptionHandler
 		StringWriter sw = new StringWriter();
 		ex.printStackTrace(new PrintWriter(sw));
 		Log.f(sw.toString(), LOG_TYPE.ERROR, true);
-
-		final Runtime runtime = Runtime.getRuntime();
-		final long usedMemInMB=(runtime.totalMemory() - runtime.freeMemory()) / 1048576L;
-		final long maxHeapSizeInMB=runtime.maxMemory() / 1048576L;
-		final long availHeapSizeInMB = maxHeapSizeInMB - usedMemInMB;
-
-		Log.f("usedMemInMB : "+usedMemInMB+", maxHeapSizeInMB : "+maxHeapSizeInMB+", availHeapSizeInMB : "+availHeapSizeInMB, LOG_TYPE.ERROR, true);
-
-		ActivityManager am  = (ActivityManager)mContext.getSystemService(Context.ACTIVITY_SERVICE);
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.FROYO)
-		{
-			am.killBackgroundProcesses(mContext.getPackageName());
-		}
-
-
 		((Activity) mContext).moveTaskToBack(true); 
 		((Activity) mContext).finish(); 
 		Process.killProcess(Process.myPid());
