e53442a1366c03d91abb011ffc74e0c76daad2f2
정재현 <only340@gmail.com>
2021-11-03 08:55:54 +0900

    [수정사항] - Gradle 세팅 변경

--- app/src/main/java/com/littlefox/logmonitor/common/Const.java
+++ app/src/main/java/com/littlefox/logmonitor/common/Const.java
@@ -4,7 +4,7 @@ import android.os.Environment;
 
 public class Const
 {
-	public final static String PATH_SDCARD	= Environment.getExternalStorageDirectory().getAbsolutePath();
+	public final static String PATH_SDCARD	= Environment.getExternalStorageDirectory().getAbsolutePath() + "";
 	public final static String PATH_ROOT	= PATH_SDCARD + "/LittleFox/Log/";
 	public final static String CONTENT_URL 	= "content.html";
 	public  static String LOG_FILE			= PATH_ROOT + "log.txt";
